9ffc140b84176c1dfbeda161ef17d534ca9e66c9
DSK1113205 <firat.gursoy@agesa.com.tr>
2023-06-24 19:16:27 +0300

    fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,6 @@ module com.gnosis.swaggerhelperui {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires com.fasterxml.jackson.databind;
-    requires swagger.models;
     requires org.json;
 
     opens com.gnosis.swaggerhelperui to javafx.fxml;
